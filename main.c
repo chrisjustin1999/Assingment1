fc8678639f368316073c2d9fd81b8b762a400c96
Chris  Justin <chrisjustin1999@gmail.con>
2019-04-03 08:37:43 +0000

    Rotation Cipher from Lab 6 - 03/04/19

--- main.c
+++ main.c
@@ -1,2 +1,15 @@
-aaaaaaaaaaahhhhhhhhhhhhhhh
-/sjdflhgS;LDHCLYI
+//Rotation Cipher Encyrption of a message
+#include <stdio.h>
+
+int main () {
+   char input[] = "hai"; // input to be shifted by the key number and printed byt the printf function
+   int string_length = 3;
+   int key = 5;
+   for (int i = 0; i < string_length; i++) {
+       printf("Input: %d %c\t\t", input[i], input[i]); //prints the input at line 5 in ASCII 
+       //...and print the letter next to it (thus why there is the 2 "input[i]'s")
+       input[i] += key;
+       printf("Encrypt: %d %c\n", input[i], input[i]); //prints the input which has been shifted an amount dictated by the key 
+       //...and prints which letter it has been shifted to next to it (thus why there is the 2 "input[i]'s")
+   }
+}
