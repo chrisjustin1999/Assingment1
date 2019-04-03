2636671dfae6fc0e625bb99d72175af59df58933
Chris  Justin <chrisjustin1999@gmail.con>
2019-04-03 09:02:37 +0000

    End of week 6 lab - 03/04/19

--- main.c
+++ main.c
@@ -2,9 +2,10 @@
 #include <stdio.h>
 
 int main () {
-   char input[] = "hai"; // input to be shifted by the key number and printed byt the printf function
-   int string_length = 3;
-   int key = 5;
+   char input[100]; //input[''] determines the amount of memory (bits) allocated to your char input or the words that you type in to be encrypted
+   scanf("%s", input); //open terminal and type in ./a.out to type in your code
+   int string_length = 3; //the amount of letters that the console will read and encrypt from your chosen word/sentence
+   int key = 5; //the amount of 
    for (int i = 0; i < string_length; i++) {
        printf("Input: %d %c\t\t", input[i], input[i]); //prints the input at line 5 in ASCII 
        //...and print the letter next to it (thus why there is the 2 "input[i]'s")
